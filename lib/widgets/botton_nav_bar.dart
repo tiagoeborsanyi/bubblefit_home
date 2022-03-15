e256f2d0dbf2ef3d023efb18ac541d3a7fd7f011
Tiago Emerick <tiagoeborsanyi@gmail.com>
2022-03-15 15:57:09 -0300

    created bottonNavBar

--- lib/widgets/botton_nav_bar.dart
+++ lib/widgets/botton_nav_bar.dart
@@ -12,17 +12,56 @@ class BottonNavBar extends StatelessWidget {
       color: bSecondaryColor,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [],
+        children: [
+          BottonNavItem(
+              title: 'Home', press: () {}, icon: Icons.home, isActive: true),
+          BottonNavItem(
+              title: 'Workouts', press: () {}, icon: Icons.fitness_center),
+          BottonNavItem(
+              title: 'Estatistica', press: () {}, icon: Icons.graphic_eq),
+          BottonNavItem(
+              title: 'Configurações', press: () {}, icon: Icons.settings)
+        ],
       ),
     );
   }
 }
 
 class BottonNavItem extends StatelessWidget {
-  const BottonNavItem({Key? key}) : super(key: key);
+  final String title;
+  final VoidCallback press;
+  final bool isActive;
+  final IconData icon;
+
+  const BottonNavItem({
+    Key? key,
+    required this.title,
+    required this.press,
+    required this.icon,
+    this.isActive = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: press,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Icon(
+            icon,
+            color: !isActive ? Colors.grey.shade400 : Colors.white,
+            size: 20,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              color: !isActive ? Colors.grey.shade400 : Colors.white,
+              fontSize: 13,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
